17e04ff575baf32d0448d4e0c8a4f37d0e8bf00c
lionking917 <lionking917@outlook.com>
2020-11-19 05:11:30 -0800

    first commit

---
+++ resources/js/routes.js
@@ -0,0 +1,7 @@
+import HomeComponent from "./components/HomeComponent.vue";
+
+const routes = [
+    { path: '/', component: HomeComponent, name: 'home' }
+];
+
+export default routes;
---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="csrf-token" content="{{csrf_token()}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/libs/bootstrap/css/bootstrap.min.css')}}"/>
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="content">
+            <div id="app">
+            </div>
+        </div>
+    </div><!-- #wrapper -->
+    <!-- jQuery -->
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('assets/libs/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key='{{ env('GOOGLE_API_KEY') }}'&libraries=places&language={{\Illuminate\Support\Facades\App::getLocale()}}"></script>
+</body>
+</html>
